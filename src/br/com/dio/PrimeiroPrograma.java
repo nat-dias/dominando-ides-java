65a14dbf274f861f4b05ae6a74af0d07a386c0a3
Natalia Dias Queiroz <nattaliadiasqueiroz@gmail.com>
2022-07-16 21:47:12 -0300

    criando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -5,10 +5,46 @@ import br.com.dio.model.Gato;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
-
         System.out.println(gato);
-        /*int a = 5;
-        int b = 3;
-        System.out.println("Hello World " + (a+b));*/
+
+        Livro livro = new Livro("ooo", 111);
+        System.out.println(livro);
     }
 }
+
+class Livro {
+    private String nome;
+    private Integer numPage;
+
+
+
+    public Livro(String nome, Integer numPage) {
+        this.nome = nome;
+        this.numPage = numPage;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPage() {
+        return numPage;
+    }
+
+    public void setNumPage(Integer numPage) {
+        this.numPage = numPage;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPage=" + numPage +
+                '}';
+    }
+}
+
